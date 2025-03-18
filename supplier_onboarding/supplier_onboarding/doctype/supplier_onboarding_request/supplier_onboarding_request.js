426075902947e2124fb9864358d9feb6ee265c0e
manyam0258 <manyam.surendhranath@gmail.com>
2025-03-18 11:53:07 +0000

    initial commit - Supplier onboarding request doctype and webform

---
+++ supplier_onboarding/supplier_onboarding/doctype/supplier_onboarding_request/supplier_onboarding_request.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, surendhranath and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Supplier Onboarding Request", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ supplier_onboarding/supplier_onboarding/doctype/supplier_payment_terms_entry/supplier_payment_terms_entry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, surendhranath and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Supplier Payment Terms Entry", {
+// 	refresh(frm) {
+
+// 	},
+// });
